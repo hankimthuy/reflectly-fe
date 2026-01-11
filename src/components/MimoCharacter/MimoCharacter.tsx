9dc9484241e9172ecf678e8703e17c3a898f88e3
Han Kim Thuy - Water <hanthuy200300@gmail.com>
2026-01-11 17:08:00 +0700

    feat: Add Mimo character component with animated glow effect and image

--- src/components/MimoCharacter/MimoCharacter.tsx
+++ src/components/MimoCharacter/MimoCharacter.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './MimoCharacter.scss';
-import { LuActivity } from 'react-icons/lu';
+import mimoImage from '../../assets/mimo.gif';
 
 interface MimoCharacterProps {
   theme?: 'bridge' | 'inner' | 'outer';
@@ -10,7 +10,17 @@ interface MimoCharacterProps {
 const MimoCharacter = ({ theme = 'bridge', className = '' }: MimoCharacterProps) => {
   return (
     <div className={`mimo-character ${className}`}>
-      
+      <div className={`mimo-character__body mimo-character__body--${theme}`}>
+         <img 
+            src={mimoImage} 
+            alt="Mimo Mascot" 
+            className="mimo-character__image"
+         />
+         
+         <div className="mimo-character__glow"></div>
+      </div>
+
+      <div className="mimo-character__shadow"></div>
     </div>
   );
 };
